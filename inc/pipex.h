7d69701d911792ab0ec5bb9ccf9ade7f64583f91
Cristina <cristinarlr@gmail.com>
2024-01-30 10:14:00 +0100

    structure added at header file

--- inc/pipex.h
+++ inc/pipex.h
@@ -6,7 +6,7 @@
 /*   By: Cristina <Cristina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 12:56:15 by crramire          #+#    #+#             */
-/*   Updated: 2024/01/26 14:10:53 by Cristina         ###   ########.fr       */
+/*   Updated: 2024/01/29 13:29:13 by Cristina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,36 @@
 # include "../src/ft_printf/ft_printf.h"
 
 /* ----- MESSAGES ----- */
+# define USAGE "Usage: ./pipex infile cmd1 cmd2 outfile\n"
+
 
 /* ----- CONSTANTS ----- */
+
+enum e_fd_events{
+	READ = 0,
+	WRITE
+};
+
+enum e_exit_code{
+	ERROR = 0,
+	NO_ERROR = 1
+};
+
+typedef struct s_pipex
+{
+	int		argc;
+	char	**argv;
+	char	**env;
+	char	**env_path;
+	int		fd_infile;
+	int		fd_outfile;
+	char	*input;
+	char	*output;
+	char	*cmd_full;
+	char	**cmd_args_splitted;
+	int		fd_pipe[2];
+	int		pid[2];
+}t_pipex;
 
 /* ----- STRUCT ----- */
 
