c09e6b370dc081d34f4ee089e4563819b1c26423
Cristina <cristinarlr@gmail.com>
2024-02-05 15:09:16 +0100

    execution of first command, but sth is happening, dsnt work correctly

--- src/pipex.c
+++ src/pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: crramire <crramire@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Cristina <Cristina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 12:48:47 by crramire          #+#    #+#             */
-/*   Updated: 2024/02/02 15:45:42 by crramire         ###   ########.fr       */
+/*   Updated: 2024/02/05 14:56:17 by Cristina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,39 @@
 //child 1 process
 static void	first_command(t_pipex *data)
 {
+	
+	
 	ft_printf("Dentro de first_command\n");
 	data->pid[0] = fork();
 	ft_print_pids(data->pid[0]);
-	printf("data->pid[0] = %i\n", data->pid[0]);
+	if (data->pid[0] < 0)
+		exit(ft_printf("crear función exit_program, exit parent)"));
+	if (data->pid[0] == CHILD)
+	{
+		ft_printf("Dentro de CHILD 1\n");
+		close(data->fd_pipe[READ]);
+		//meter todo lo que viene despues en exec_input_cmd(data)?;
+		if (dup2(data->fd_infile, STDIN_FILENO) < 0)
+			exit(ft_printf("1 crear función exit_program, child1 dup2, close fds"));
+		close(data->fd_infile);
+		//if (dup2(data->fd_pipe[WRITE], STDOUT_FILENO) < 0)
+		//	exit(ft_printf("2 crear función exit_program, child1 dup2, close fds"));
+		//close(data->fd_pipe[WRITE]);
+		if (execve(get_cmd_path_route(data, data->argv[2]), data->cmd_args_splitted, data->envp) == -1)
+			exit(ft_printf("ERR execve crear función exit_program, exit child"));
+		ft_printf("SUCCESS CHILD 1\n");
+	}
+	else
+		wait(NULL);
 }
+
+/* //child 2 process
+static void second_command(t_pipex *data)
+{
+
+	
+	
+} */
 
 int	pipex(t_pipex *data)
 {
