acc21a777389ad15c6e57441e1e4ef72a5d02c34
Cristina <cristinarlr@gmail.com>
2024-03-12 07:24:59 +0100

    my program considers ./pipex infile “ls -la” “wc -l” outfile as 7 arguments, it separates by spaces

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: Cristina <Cristina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 13:00:59 by crramire          #+#    #+#             */
-/*   Updated: 2024/03/12 06:57:38 by Cristina         ###   ########.fr       */
+/*   Updated: 2024/03/12 07:22:06 by Cristina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,29 +30,37 @@ void	check_leaks(void)
 	system("leaks -q pipex");
 }
 
-static void	check_program_args(int argc, char **argv, char **envp)
+static void	check_program_args(int argc, char **envp)
 {
-	(void) argv;
-
 	if (!envp)
 		exit_program(ENV, NULL);
 	if (argc != 5)
+	{
 		perror("Usage: ./pipex infile \"cmd1\" \"cmd2\" outfile\n");
+		exit(5);
+	}
 }
 
 /* Initialize data structure, checking environment, call to pipex */
 int	main(int argc, char **argv, char **envp)
 {
 	t_pipex	data;
+	int	i = 0;
 
 	//atexit(check_leaks);
 	ft_printf("%s PID: %i\n", argv[0], getpid());
-	check_program_args(argc, argv, envp);
+	while(argv[i] != NULL)
+	{
+		printf("argv[%i] = %s\n", i, argv[i]);
+		i++;
+	}
+	printf("argv[%i] = %s\n", i, argv[i]);
+	check_program_args(argc, envp);
 	data.argc = argc;
 	data.argv = argv;
 	data.envp = envp;
-	data.cmds[0] = *(argv + 2);
-	data.cmds[1] = *(argv + 3);
+	//data.cmds[0] = *(argv + 2);
+	//data.cmds[1] = *(argv + 3);
 	printf("´´´´´´´´´´´´´´´´´´´´´´´´´\nargv[2] = %s\n argv[3] = %s\n", argv[2], argv[3]);
 	if (init_data_structure(&data) == ERROR)
 	{
